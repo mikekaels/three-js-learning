13d07aa4d409a2d82534a4a7e3f35b4467d5c0c0
MIke <santo.michael@privyid.tech>
2023-11-26 22:01:26 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { Suspense } from 'react'
+import { Canvas, useLoader } from '@react-three/fiber';
+import { OrbitControls, Stars, Environment } from '@react-three/drei'
 
-function App() {
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+
+function Room() {
+  const gltf = useLoader(GLTFLoader, './room/scene.gltf')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <mesh scale={0.05}>
+      <primitive object={gltf.scene} dispose={null} />
+    </ mesh>
+
+  )
+}
+
+
+export default function App() {
+  return (
+    <div className='app'>
+      <Canvas>
+        <ambientLight intensity={2} />
+        <directionalLight color="white" position={[0, 0, 5]} />
+        <OrbitControls />
+        <Room />
+      </Canvas>
+    </div>
+  )
+
+  return (
+    <div className='app'>
+      <Canvas>
+        <OrbitControls />
+        <Stars />
+        <ambientLight intensity={0.8} />
+        <directionalLight position={[10, 15, 10]} />
+        <Box />
+        <Plane />
+      </Canvas>
     </div>
   );
 }
 
-export default App;
+function Box() {
+  return (
+    <mesh>
+      <boxGeometry attach="geometry" />
+      <meshStandardMaterial attach="material" color={"hotpink"} />
+    </mesh>
+  );
+}
+
+function Plane() {
+  return (
+    <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+      <planeGeometry attach={"geometry"} args={[5, 5]} />
+      {/* <meshStandardMaterial attach={"geometry"} color={"seofoamgreen"} /> */}
+    </mesh>
+  )
+}
